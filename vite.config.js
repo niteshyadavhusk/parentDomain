cb3a0e73ac1c1869e9e1120657c5460f7b44c2c7
Nitesh Yadav <nitesh_s_yadav@huskpowersystems.com>
2024-06-19 11:32:51 +0530

    adding config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 100000000
+  },
+  server: {
+    host: true,
+    strictPort: true,
+    port: 8000,
+    },
 })
