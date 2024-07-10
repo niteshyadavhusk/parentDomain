550dc733e84d33d4a6aac8e7c4be484fd6587d4c
Nitesh Yadav <nitesh_s_yadav@huskpowersystems.com>
2024-07-10 11:16:28 +0530

    adding react native

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,22 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  // plugins: [react()],
+  // server: {
+  //   open: true, // Automatically open the app in the browser
+  // },
+  // build: {
+  //   outDir: 'dist',
+  //   assetsDir: 'assets',
+  // },
+
   plugins: [react()],
-  server: {
-    open: true, // Automatically open the app in the browser
-  },
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
+    chunkSizeWarningLimit: 100000000
   },
+  server: {
+    host: true,
+    strictPort: true,
+    port: 8000,
+    },
 });
