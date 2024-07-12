c5a7d5d608d765acf4a8bb2223eff702fb28b58d
Nitesh Yadav <nitesh_s_yadav@huskpowersystems.com>
2024-07-12 13:19:00 +0530

    adding form

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,16 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export const Form = () => {
+    const [imageUri, setImageUri] = useState('https://tse1.mm.bing.net/th?id=OIP.7JVo-fa_E9WVVkFwa_eO6gHaD6&pid=Api&P=0&h=180');
+   const [location, setLocation] = useState(null);
+
+  
+
+  useEffect(() => {
+    const receiveMessage = (event) => {
+      try {
+        const message = JSON.parse(event.data);
+        if (message.type === 'image') {
+          setImageUri(message.data)
+          alert('Image received');
+        } else if (message.type === 'location') {
+          setLocation(message.data);
+          alert('Location received');
+        }
+      } catch (error) {
+        console.error('Error parsing message:', error);
+      }
+    }
+    window.addEventListener('message', receiveMessage);
+
+    return () => {
+      window.removeEventListener('message', receiveMessage);
+    };
+  }, []);
+
+
     return (
         <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexWrap: 'wrap', flexDirection: 'column' }}>
             <h2>Husk Power System</h2>
 
             <form>
                 <div style={{ display: 'flex', flexDirection: "column" }}>
-                    <div style={{ width: "200px", height: '200px', border: '1px solid grey', borderRadius: '50%' }}>
-                        <img src='' />
+                    <div style={{ width: "200px", height: '200px', border: '1px solid blue', borderRadius: '50%' }}>
+                        <img src={imageUri }  style={{width:'100%',height:'100%', borderRadius: '50%'}}/>
                     </div>
-                    <button >upload Img</button>
+                    <br></br>
+                    <button  onClick={() => window.ReactNativeWebView.postMessage('openCamera!')} >upload Img</button>
 
                 </div>
                 <div style={{display:'flex',flexDirection:'column',marginTop:'20px'}}>
@@ -19,7 +48,13 @@ export const Form = () => {
                 
                     <label htmlFor='Area'>City</label>
                     <input type='text' name='Area' id='Area' />
+                    <label htmlFor='Latitude'>Latitude</label>
+                    <input type="number" placeholder='Latitude' name='Latitude'/>
+                    <label htmlFor='Longitude'>Longitude</label>
+                    <input type="number" placeholder='Longitude' name='Longitude' />
                 </div>
+                <br/>
+                <button type='submit' name='Submit'>Submit</button>
             </form>
         </div>
     )
