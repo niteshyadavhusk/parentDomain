7c68b069f19c9c0c0a062bb91093ea297c4e2328
Nitesh Yadav <nitesh_s_yadav@huskpowersystems.com>
2024-07-12 16:26:37 +0530

    adding form

--- src/Form.jsx
+++ src/Form.jsx
@@ -5,6 +5,7 @@ export const Form = () => {
    const [location, setLocation] = useState(null);
    const [isNative, setIsNative] = useState(false);
   
+  
 
   useEffect(() => {
     const receiveMessage = (event) => {
@@ -15,7 +16,7 @@ export const Form = () => {
           alert('App opened in React Native WebView');
         } else if (message.type === 'image') {
           setImageUri(message.data);
-          alert('Image received');
+         
         } else if (message.type === 'location') {
           setLocation(message.data);
           alert('Location received');
@@ -37,14 +38,19 @@ export const Form = () => {
             <h2>Husk Power System</h2>
 
             <form>
-                <div style={{ display: 'flex', flexDirection: "column" }}>
+                {isNative ? <div style={{ display: 'flex', flexDirection: "column" }}>
                     <div style={{ width: "200px", height: '200px', border: '1px solid blue', borderRadius: '50%' }}>
                         <img src={imageUri }  style={{width:'100%',height:'100%', borderRadius: '50%'}}/>
                     </div>
                     <br></br>
                     <button  onClick={() => window.ReactNativeWebView.postMessage('openCamera!')} >upload Img</button>
 
+                </div>:
+                <div>
+                  <label htmlFor='img'>Image</label>
+                  <input type='file'/>
                 </div>
+                }
                 <div style={{display:'flex',flexDirection:'column',marginTop:'20px'}}>
                     <label htmlFor='name'>Name</label>
                     <input type='text' name='name' id='name' />
@@ -52,9 +58,11 @@ export const Form = () => {
                     <label htmlFor='Area'>City</label>
                     <input type='text' name='Area' id='Area' />
                     <label htmlFor='Latitude'>Latitude</label>
-                    <input type="number" placeholder='Latitude' name='Latitude'/>
+                    <input type="number" placeholder='Latitude' name='Latitude' value={location ? location.latitude :"getLatutue"}/>
                     <label htmlFor='Longitude'>Longitude</label>
-                    <input type="number" placeholder='Longitude' name='Longitude' />
+                    <input type="number" placeholder='Longitude' name='Longitude' value={location ? location.longitude :"getlongitude"} />
+                    <br/>
+                    <button onClick={()=>window.ReactNativeWebView.postMessage('getLocation!')}>getLocation</button>
                 </div>
                 <br/>
                 <button type='submit' name='Submit'>Submit</button>
