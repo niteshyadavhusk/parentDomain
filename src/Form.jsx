f9d1c538555cc982eabb01b7f867d77627a90a30
Nitesh Yadav <nitesh_s_yadav@huskpowersystems.com>
2024-07-15 14:17:20 +0530

    adding data

--- src/Form.jsx
+++ src/Form.jsx
@@ -3,13 +3,28 @@ import React, { useEffect, useState } from 'react'
 export const Form = () => {
   const [imageUri, setImageUri] = useState(
     localStorage.getItem('imageUri') ||
-      'https://tse1.mm.bing.net/th?id=OIP.7JVo-fa_E9WVVkFwa_eO6gHaD6&pid=Api&P=0&h=180'
+    'https://tse1.mm.bing.net/th?id=OIP.7JVo-fa_E9WVVkFwa_eO6gHaD6&pid=Api&P=0&h=180'
   );
   const [location, setLocation] = useState(
     JSON.parse(localStorage.getItem('location')) || null
   );
 
+  const [userDetails, setDetails] = useState({
+    name: '',
+    city: ''
+  })
+
   const [isNative, setIsNative] = useState(false);
+
+
+
+  const handleChangedata = (e) => {
+    const { name, value } = e.target;
+    setDetails((prevState) => ({
+      ...prevState,
+      [name]: value
+    }));
+  }
   // const [web, setWeb] = useState(false)
   // const [environment, setEnvironment] = useState('');
 
@@ -35,7 +50,7 @@ export const Form = () => {
     const receiveMessage = (event) => {
       try {
         const message = JSON.parse(event.data);
-         if (message.type === 'image') {
+        if (message.type === 'image') {
           setImageUri(message.data);
 
         } else if (message.type === 'location') {
@@ -56,7 +71,7 @@ export const Form = () => {
   // useEffect(() => {
   //   const timer = setTimeout(() => {
   //     if (isNative === "native") {
-       
+
   //       setWeb(true);
   //     } else {
   //       setWeb(false);
@@ -65,44 +80,59 @@ export const Form = () => {
 
   //   return () => clearTimeout(timer); // Cleanup the timer
   // }, [isNative]);
-const sendMessageNagive= async()=>{
+  const sendMessageNagive = async () => {
     await window.ReactNativeWebView.postMessage('openCamera!')
-}
+  }
 
-const sendMessageLocation= async(e)=>{
-  e.preventDefault();
-  await window.ReactNativeWebView.postMessage('getLocation!')
-}
- 
-  return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexWrap: 'wrap', flexDirection: 'column' }}>
-      <h2>Husk Power System</h2>
+  const sendMessageLocation = async (e) => {
+    e.preventDefault();
+    await window.ReactNativeWebView.postMessage('getLocation!')
+  }
 
-      <form>
-         <div style={{ display: 'flex', flexDirection: "column" }}>
-          <div style={{ width: "200px", height: '200px', border: '1px solid blue', borderRadius: '50%' }}>
-            <img src={imageUri} style={{ width: '100%', height: '100%', borderRadius: '50%' }} />
-          </div>
-          <br></br>
-          <button onClick={sendMessageNagive} >upload Img</button>
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+    const dataObject = {
+      image: imageUri,
+      location: location,
+      name: userDetails.name,
+      city: userDetails.city
+    };
+    console.log(dataObject);
+    alert(JSON.stringify(dataObject));
+  };
 
-        </div> 
-        <div style={{ display: 'flex', flexDirection: 'column', marginTop: '20px' }}>
-          <label htmlFor='name'>Name</label>
-          <input type='text' name='name' id='name' />
+console.log(userDetails.name)
 
-          <label htmlFor='Area'>City</label>
-          <input type='text' name='Area' id='Area' />
-          <label htmlFor='Latitude'>Latitude</label>
-          <input type="number" placeholder='Latitude' name='Latitude' value={location ? location.latitude : "getLatutue"} />
-          <label htmlFor='Longitude'>Longitude</label>
-          <input type="number" placeholder='Longitude' name='Longitude' value={location ? location.longitude : "getlongitude"} />
-          <br />
-          <button onClick={sendMessageLocation}>getLocation</button>
+return (
+  <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexWrap: 'wrap', flexDirection: 'column' }}>
+    <h2>Husk Power System</h2>
+
+    <form>
+      <div style={{ display: 'flex', flexDirection: "column" }}>
+        <div style={{ width: "200px", height: '200px', border: '1px solid blue', borderRadius: '50%' }}>
+          <img src={imageUri} style={{ width: '100%', height: '100%', borderRadius: '50%' }} />
         </div>
+        <br></br>
+        <button onClick={sendMessageNagive} >upload Img</button>
+
+      </div>
+      <div style={{ display: 'flex', flexDirection: 'column', marginTop: '20px' }}>
+        <label htmlFor='name'>Name</label>
+        <input type='text' name='name' id='name' value={userDetails.name} onChange={handleChangedata} />
+
+        <label htmlFor='Area'>City</label>
+        <input type='text' name='city' id='Area' value={userDetails.city} onChange={handleChangedata} />
+        <label htmlFor='Latitude'>Latitude</label>
+        <input type="number" placeholder='Latitude' name='Latitude' value={location ? location.latitude : "getLatutue"} readOnly />
+        <label htmlFor='Longitude'>Longitude</label>
+        <input type="number" placeholder='Longitude' name='Longitude' value={location ? location.longitude : "getlongitude"} readOnly />
         <br />
-        <button type='submit' name='Submit'>Submit</button>
-      </form>
-    </div>
-  )
+        <button onClick={sendMessageLocation}>getLocation</button>
+      </div>
+      <br />
+      <button type='submit' name='Submit' >Submit</button>
+    </form>
+  </div>
+)
 }
